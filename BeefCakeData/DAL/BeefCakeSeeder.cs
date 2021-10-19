e74166a5a99c3801db0a51192aaeed28c3bf8093
SylwiaLeung <longdepati@gmail.com>
2021-10-19 13:18:39 +0200

    feature_seeder add_proper_data

--- BeefCakeData/DAL/BeefCakeSeeder.cs
+++ BeefCakeData/DAL/BeefCakeSeeder.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using BeefCakeData.Model;
+using BeefCakeData.Utilities;
 using Microsoft.EntityFrameworkCore;
 
 namespace BeefCakeData.DAL
@@ -11,37 +13,73 @@ namespace BeefCakeData.DAL
             List<User> Users = new();
             Users.Add(new User()
             {
-                ID = 1,
-                Name = "Mark",
-                DateOfBirth = DateTime.Parse("1988-05-12"),
-                Height: 180,
-                Gender: Gender.Male
+                Id = 1,
+                Name = "Grzesiek",
+                DateOfBirth = DateTime.Parse("1992-07-09"),
+                Height = 175,
+                Gender = Gender.M
             });
             Users.Add(new User()
             {
-                ID = 2,
-                Name = "Susan",
-                DateOfBirth = DateTime.Parse("1991-10-01"),
-                Height: 165,
-                Gender: Gender.Female
+                Id = 2,
+                Name = "Piotr",
+                DateOfBirth = DateTime.Parse("1982-07-04"),
+                Height = 185,
+                Gender = Gender.M
+            });
+            Users.Add(new User()
+            {
+                Id = 3,
+                Name = "Łukasz",
+                DateOfBirth = DateTime.Parse("1994-06-05"),
+                Height = 185,
+                Gender = Gender.M
+            });
+            Users.Add(new User()
+            {
+                Id = 4,
+                Name = "Sylwia",
+                DateOfBirth = DateTime.Parse("1992-11-14"),
+                Height = 170,
+                Gender = Gender.F
             });
 
             List<Measurement> Measurements = new();
             Measurements.Add(new Measurement()
             {
-                ID = 1,
+                Id = 1,
                 UserId = 1,
-                Calories =,
-                Date =,
-                Weight =
+                Calories = 3000,
+                Date = DateTime.Now,
+                Weight = 68.5m,
+                Bmi = 22.2m
             });
             Measurements.Add(new Measurement()
             {
-                ID = 1,
+                Id = 1,
                 UserId = 2,
-                Calories =,
-                Date = ,
-                Weight =
+                Calories = 3500,
+                Date = DateTime.Now,
+                Weight = 80,
+                Bmi = 23.3m
+            });
+            Measurements.Add(new Measurement()
+            {
+                Id = 1,
+                UserId = 3,
+                Calories = 2000,
+                Date = DateTime.Now,
+                Weight = 78,
+                Bmi = 22.7m
+            });
+            Measurements.Add(new Measurement()
+            {
+                Id = 1,
+                UserId = 4,
+                Calories = 1900,
+                Date = DateTime.Now,
+                Weight = 54.0m,
+                Bmi = 18.6m
             });
 
 
