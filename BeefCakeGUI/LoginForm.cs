eaca3d8f95bb0156746d37e09cfa2364f8858ef3
LCzunko <LCzunko@gmail.com>
2021-10-19 15:14:03 +0200

    Implement LoginForm events.

--- BeefCakeGUI/LoginForm.cs
+++ BeefCakeGUI/LoginForm.cs
@@ -7,14 +7,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BeefCakeData.DAL.DAOInterface;
+using BeefCakeData.Model;
 
 namespace BeefCakeGUI
 {
     public partial class LoginForm : Form
     {
-        public LoginForm()
+        private User activeUser;
+        private Panel activePanel;
+
+        public LoginForm(IUserDao userDao)
         {
             InitializeComponent();
+            LoadLoginPanelData();
         }
+        private void userAddButton_Click(object sender, EventArgs e)
+        {
+            this.SuspendLayout();
+            //TODO LoadUserPanelData();
+            //SwitchPanel(userPanel);
+            this.ResumeLayout();
+        }
+
+        private void userConfirmButton_Click(object sender, EventArgs e)
+        {
+            this.SuspendLayout();
+            activeUser = (User)usersComboBox.SelectedItem;
+            //TODO LoadGraphPanelData();
+            //SwitchPanel(graphPanel);
+            this.ResumeLayout();
+        }
+
+        private void LoadLoginPanelData()
+        {
+            //TODO usersComboBox.DataSource = userDao.ReadAll();
+            usersComboBox.DisplayMember = "Name";
+        }
+
+        private void SwitchPanel(Panel panelToSwitch)
+        {
+            activePanel.Enabled = false;
+            activePanel.Visible = false;
+            activePanel = panelToSwitch;
+            activePanel.Enabled = true;
+            activePanel.Visible = true;
+        }
+
     }
 }
