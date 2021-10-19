d5bdedff57f5563299f5f5a38b3f4870d8c22e1c
pikuch <55052790+pikuch@users.noreply.github.com>
2021-10-19 13:54:05 +0200

    Add UserController stub

---
+++ BeefCakeLogic/UserController.cs
@@ -0,0 +1,18 @@
+﻿using BeefCakeData.Model;
+using System;
+
+namespace BeefCakeLogic
+{
+    public class UserController
+    {
+        void EditUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        void AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
