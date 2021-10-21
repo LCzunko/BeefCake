8b8253fcdddbaa63daee4e54ddfddd04ccd5d2fa
LCzunko <LCzunko@gmail.com>
2021-10-21 14:06:05 +0200

    Change form icon.

--- BeefCakeGUI/Properties/Resources.Designer.cs
+++ BeefCakeGUI/Properties/Resources.Designer.cs
@@ -91,6 +91,16 @@ namespace BeefCakeGUI.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon CartmanCopHead_Icon {
+            get {
+                object obj = ResourceManager.GetObject("CartmanCopHead_Icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap cartmanPointing {
