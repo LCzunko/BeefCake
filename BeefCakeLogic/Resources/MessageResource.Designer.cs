ac07bd1ca01e8bfd3725dcb345adea88a779e5a4
LCzunko <LCzunko@gmail.com>
2021-10-21 17:35:37 +0200

    Rearrange file/folder structure, remove unused dependencies.

--- BeefCakeLogic/Resources/MessageResource.Designer.cs
+++ BeefCakeLogic/Resources/MessageResource.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BeefCakeLogic {
+namespace BeefCakeLogic.Resources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace BeefCakeLogic {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BeefCakeLogic.MessageResource", typeof(MessageResource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BeefCakeLogic.Resources.MessageResource", typeof(MessageResource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
